b36ad4b18985b1b03607955d5bc3f7c9e9622c8b
starwaver <jackjack_liu@hotmail.com>
2025-01-03 15:04:54 -0700

    added a second room

--- Assets/Scenes/GameObjectManager.cs
+++ Assets/Scenes/GameObjectManager.cs
@@ -12,6 +12,18 @@ public class GameObjectManager : MonoBehaviour
 
     private List<GameObject> gameObjects = new List<GameObject>();
 
+    public static int CurrentlySelectedRoom;
+
+    public List<GameObject> room_1_gameobjects = new List<GameObject>();
+
+    public Transform room_1_camera_transform;
+
+    [Space(10)]
+
+    public List<GameObject> room_2_gameobjects = new List<GameObject>();
+
+    public Transform room_2_camera_transform;
+
     private void Awake()
     {
         // Singleton setup
@@ -23,6 +35,11 @@ public class GameObjectManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject); // Optional: Persist across scenes
+    }
+
+    private void Start()
+    {
+        SetRoom(1);
     }
 
     // Method to add a game object to the manager
@@ -50,9 +67,63 @@ public class GameObjectManager : MonoBehaviour
         SetSelectedObject(selectedObject);
     }
 
+    public void RemoveAllGameObject()
+    {
+        while (gameObjects.Count > 0)
+        {
+            RemoveLastGameObject();
+        }
+    }
+
     public void SetSelectedObject(GameObject selectedObject)
     {
         CurrentlySelectedObject = selectedObject;
         OnSelectedObjectChanged?.Invoke(selectedObject);
+    }
+
+    public void SetRoom(int index)
+    {        
+        if (CurrentlySelectedRoom == index)
+        {
+            return;
+        }
+        else if (index == 1)
+        {
+            foreach (GameObject obj in room_1_gameobjects)
+            {
+                obj.SetActive(true);
+            }
+
+            foreach (GameObject obj in room_2_gameobjects)
+            {
+                obj.SetActive(false);
+            }
+
+            Camera.main.transform.position = room_1_camera_transform.position;
+            Camera.main.transform.rotation = room_1_camera_transform.rotation;
+
+            RemoveAllGameObject();
+
+            CurrentlySelectedRoom = 1;
+        }
+        else if (index == 2)
+        {
+            foreach (GameObject obj in room_1_gameobjects)
+            {
+                obj.SetActive(false);
+            }
+
+            foreach (GameObject obj in room_2_gameobjects)
+            {
+                obj.SetActive(true);
+            }
+
+            Camera.main.transform.position = room_2_camera_transform.position;
+            Camera.main.transform.rotation = room_2_camera_transform.rotation;
+
+            RemoveAllGameObject();
+
+            CurrentlySelectedRoom = 2;
+        }
     }
 }
